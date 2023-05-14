b1f94771fb6bf4f3f2f8719e6e2f03a17b1217c6
Saryev Rustam <r.saryev@indriver.com>
2023-05-14 14:30:33 +0300

    update deploy.bash

--- deployment/deploy.bash
+++ deployment/deploy.bash
@@ -1,18 +1,14 @@
 #!/bin/bash
-
 # Install Node.js version 18.16.0 if not already installed
 if ! node -v | grep -q "^v18\."; then
-  echo "Node.js is not installed. Installing Node.js..."
+  echo "🚀 Node.js is not installed. Installing Node.js..."
   curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
   source ~/.nvm/nvm.sh
   nvm install v18.16.0
 fi
 
-# Install auto-copilot-cli if not already installed
-if ! command -v auto-copilot-cli >/dev/null 2>&1; then
-  echo "auto-copilot-cli is not installed. Installing auto-copilot-cli..."
+if ! npm list -g | grep -q "auto-copilot-cli"; then
+  echo "🚀 auto-copilot-cli is not installed. Installing auto-copilot-cli..."
   npm install -g auto-copilot-cli
 fi
 
-echo "auto-copilot-cli has been successfully installed! You can now use the copilot command."
-echo "To get started, run copilot --help"
